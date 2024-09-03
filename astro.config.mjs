4ade66cc9f88a74e8664402eedbe256922c4d394
serburyat <tema.anosov@yandex.ru>
2024-09-03 05:31:32 +0300

    navbar as component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import tunnel from "astro-tunnel";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tunnel()]
+  integrations: [tunnel(), icon()]
 });
