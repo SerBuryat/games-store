87ed8b608ade6d647c4057c11f6d2bc6d362706c
serburyat <tema.anosov@yandex.ru>
2024-09-09 04:38:23 +0300

    make Nav.astro as component based structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 import tunnel from "astro-tunnel";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tunnel(), icon()]
+  integrations: [tunnel()]
 });
