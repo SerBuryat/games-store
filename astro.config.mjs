8c0321d8138856b46e1d27a7ba83bf5c65083ae5
serburyat <tema.anosov@yandex.ru>
2024-09-24 04:53:04 +0300

    node js as server adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tunnel(),
-    mdx(),
-    react(),
-    tailwind({
-      nesting: true,
-    })
-  ]
+  integrations: [tunnel(), mdx(), react(), tailwind({
+    nesting: true
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
